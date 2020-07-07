a7e6d082509615f96d696a27fae3d9b673810f90
Salman Ali Banani <salman.banani@readify.net>
2020-07-07 12:26:30 +1000

    Debuggin powershell

--- Templates/SetKeys.ps1
+++ Templates/SetKeys.ps1
@@ -1,2 +1,7 @@
+Write-Host "I am here xx"
+Write-Host $env:ServiceBusTemplateOutput
 $armOutputObj = $env:ServiceBusTemplateOutput | convertfrom-json
 $env:ServiceBusConnectionString = $armOutputObj.namespaceConnectionString.value
+Write-Host "I am here xy"
+Write-Host "$env:ServiceBusConnectionString"
+Write-Host $env:ServiceBusConnectionString
